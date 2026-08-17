acf4a17108ab896bcd435ce35c19614a1f5137a1
Molly-Shelby <segunmosesabc@gmail.com>
2026-08-17 23:32:14 +0100

    Create Intro2.js with personal information

---
+++ Intro2.js
@@ -0,0 +1,8 @@
+// intro.js
+
+const name = "Ayoola Segun";
+const age = 20; /
+const favoriteLanguage = "JavaScript"; 
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
